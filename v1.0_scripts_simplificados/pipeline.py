2beb1c2f0318551dacb2938121911752bdb194b5
EmilioKolo <emilio_193@hotmail.com>
2024-03-27 12:16:26 -0300

    Agrego abrir resultados RNA-seq y pasar de entrez a ensembl

--- v1.0_scripts_simplificados/pipeline.py
+++ v1.0_scripts_simplificados/pipeline.py
@@ -1,5 +1,5 @@
 
-ib3 = False; 
+ib3 = True; 
 http_proxy_ib3 = 'http://proxy.fcen.uba.ar:8080'; 
 https_proxy_ib3 = 'https://proxy.fcen.uba.ar:8080'; 
 ftp_proxy_ib3 = 'ftp://proxy.fcen.uba.ar:8080'; 
@@ -48,8 +48,13 @@ mm10 = EnsemblRelease(102, species='mouse');
 # X Agregar info de genes cercanos a peaks y sitios de union
 # X Abrir resultados RNAseq
 # Seleccionar genes RNAseq
-    # Ver desde scripts 10- para atras
+    # * Ver en 9-ClasificacionPeaks3.py, 8-TraduccionRatonHumano.py, 8-ConteoGenesDupaysRNAseq.py y 7-ClasificacionPeaks2.py
+    # X Extraer genes RNAseq
+    # X Pasarlos a ID de Ensembl
+    # Seleccionar genes en M_genes por lista RNAseq y agregar columna
+    # Agregar info de updown a M_genes
 # Generar secuencias para MEME-ChIP
+    # Ver 4-GeneracionFastaMEME.py en Scripts_0_16
 # Agregar cosas relacionadas a estudiar varios factores de transcripcion
 ###
 '''
@@ -59,7 +64,7 @@ mm10 = EnsemblRelease(102, species='mouse');
 
 # Direcciones de archivos y outputs para ib3 y casa
 path_git_casa = 'C:\\Users\\Admin\\Documents\\Scripts\\Git\\ChIP-seq-analysis-scripts\\v1.0_scripts_simplificados\\'; 
-path_git_ib3 = 'C:\\Users\\emili\\Documents\\Archivos nube\\Git\\ChIP-seq-analysis-scripts\\v1.0_scripts_simplificados\\'; 
+path_git_ib3 = 'C:\\Users\\emili\\Documents\\Archivos nube\\Git\\ChIPseqanalysisscripts\\v1.0_scripts_simplificados\\'; 
 path_fasta_casa = 'D:\\ArchivosDoctorado\\Genomas\\'; 
 path_fasta_ib3 = 'X:\\Genomas\\'; 
 path_output_dump_casa = 'D:\\ArchivosDoctorado\\Output_dump\\'; 
@@ -89,6 +94,7 @@ nom_pssm_nkx25_human = 'NKX25_HUMAN.H11MO.0.B.pcm';
 nom_pssm_nkx25_mouse = 'NKX25_MOUSE.H11MO.0.A.pcm'; 
 score_mult = 0.9; # Multiplicador del score maximo de pssm que se usa como cutoff
 organism = 'human'; # human o mouse
+test_used = 0; # 0 para correr todo completo, numeros mayores a 0 para correr subsets de largo test_used
 
 
 
@@ -103,18 +109,20 @@ def _main():
         genoma_usado = hg19; 
         nom_genoma_usado = 'hg19'; 
         nom_bed_usado = 'Anderson2018'; 
-        nom_rnaseq_usado = 'Anderson2018_RNAseq'; 
-        id_col_usado = 0; 
-        updown_col_usado = 4; 
+        nom_rnaseq_usado = 'Anderson2018_RNAseq_source'; 
+        id_col_usado = 1; 
+        updown_col_usado = 3; 
+        l_translate = [0, hg19]; 
         nom_output = 'anderson_full'; 
     elif organism.lower()=='mouse' or organism.lower()=='raton':
         pssm_usado = abrir_pssm(nom_pssm_nkx25_mouse, path_arch=path_pwm_mouse); 
         genoma_usado = mm9; 
         nom_genoma_usado = 'mm9'; 
         nom_bed_usado = 'Dupays2015'; 
-        nom_rnaseq_usado = 'Dupays2015_RNAseq'; 
-        id_col_usado = 4; 
-        updown_col_usado = 6; 
+        nom_rnaseq_usado = 'Dupays2015_RNAseq_source'; 
+        id_col_usado = 12; 
+        updown_col_usado = 1; 
+        l_translate = []; 
         nom_output = 'dupays_full'; 
     else:
         print('Organismo ' + str(organism) + ' no reconocido.')
@@ -123,7 +131,7 @@ def _main():
     
     M_peaks, M_su, M_genes = pipeline_generador(nom_bed_usado, nom_rnaseq_usado, nom_output, genoma_usado, nom_genoma_usado, pssm_usado, score_cutoff_pssm, 
                                                 path_bed=path_git_main, path_out=path_out_main, path_fasta=path_fasta_main, dist_max_gen=dist_max_main, 
-                                                L_su=L_confirmados, test_mode=0, id_col_rnaseq=id_col_usado, updown_col_rnaseq=updown_col_usado); 
+                                                L_su=L_confirmados, test_mode=test_used, id_col_rnaseq=id_col_usado, updown_col_rnaseq=updown_col_usado, l_translate=l_translate); 
 
     ### Display
     #for i in M_peaks:
@@ -133,17 +141,20 @@ def _main():
 
 
 def pipeline_generador(nom_bed, nom_rnaseq, nom_out_base, genoma_ensembl, nombre_genoma, pssm, score_cutoff, path_bed='', path_out='', path_fasta='', dist_max_gen=1000000, L_su=[], 
-                       id_col_rnaseq=0, updown_col_rnaseq=1, test_mode=0):
+                       id_col_rnaseq=0, updown_col_rnaseq=1, l_translate=[], test_mode=0):
     '''Pipeline desde abrir archivos .bed y resultados RNA-seq hasta generar tablas de sitios y genes que se usan por otros scripts.
     genoma_ensembl es el elemento genoma de ensembl, usado para buscar genes cerca de peaks en el archivo .bed.
     dist_max_gen es la distancia maxima a la que se buscan genes cercanos.
     L_su es una lista de secuencias consideradas sitios de union.
     pssm es una matriz de puntaje para secuencias (FORMATO UNIFICADO POR UNA FUNCION)
     score_cutoff es el score minimo aceptado para una secuencia correcta de pssm (uso 90% del puntaje maximo posible).
-    test_mode define si se hace una prueba con una seleccion de peaks (se hace si es un numero distinto de 0)'''
+    l_translate se usa para traducir nombres de genes extraidos de archivo nom_rnaseq. Si esta vacio, no se traduce. Sino, tiene que tener columna de nombre de gen y genoma usado para confirmar Ensembl ID.
+    test_mode define si se hace una prueba con una seleccion de peaks (se hace si es un numero distinto de 0).'''
 
     # Abro archivo .bed
     M_peaks = abrir_bed(nom_bed, path_arch=path_bed); 
+    # Abro los resultados de RNA-seq
+    M_rnaseq = abrir_rnaseq(nom_rnaseq, path_arch=path_bed, sep=';', ext='.csv', id_col=id_col_rnaseq, updown_col=updown_col_rnaseq, traducir=l_translate); 
     # Obtengo la secuencia de todos los archivos fasta del genoma con elementos SeqIO
     dict_chr_n = seqio_chr_n(M_peaks, nombre_genoma, path_fasta); 
     ### Reviso si hago test
@@ -156,7 +167,7 @@ def pipeline_generador(nom_bed, nom_rnaseq, nom_out_base, genoma_ensembl, nombre
     # Inicializo la matriz de genes
     M_genes = []; 
     # Defino largo de M_peaks
-    len_peaks=len(M_peaks)
+    len_peaks=len(M_peaks); 
     # Recorro M_peaks para trabajar sobre cada peak
     for i in range(len_peaks):
         curr_peak = M_peaks[i]; 
@@ -185,13 +196,13 @@ def pipeline_generador(nom_bed, nom_rnaseq, nom_out_base, genoma_ensembl, nombre
         if ((i+1)%100==0) or i==0:
             print('Avance: ' + str(i+1) + ' de ' + str(len_peaks))
         ###
-    # Abro los resultados de RNA-seq
-    M_rnaseq = abrir_rnaseq(nom_rnaseq, path_arch=path_bed, sep=';', ext='.csv', id_col=id_col_rnaseq, updown_col=updown_col_rnaseq); 
     ### FALTA
     # X Agregar info de genes cercanos a peaks y sitios de union
     # X Abrir resultados RNAseq
     # Seleccionar genes RNAseq
-        # Pasar IDs a Ensembl
+        # X Pasar IDs a Ensembl
+        # X Probar con genome.genes_by_name(gene_name)
+        # Ver en 9-ClasificacionPeaks3.py, 8-TraduccionRatonHumano.py, 8-ConteoGenesDupaysRNAseq.py y 7-ClasificacionPeaks2.py
     ###
     M_genes = eliminar_duplicados(M_genes); 
     M_su = guardar_matriz(nom_out_base+'_sitios_union', M_su, path_out=path_out, l_head=['chr_n', 'pos_ini', 'pos_end', 'seq', 'source', 'score_pssm']); 
@@ -459,32 +470,91 @@ def abrir_pssm(nom_arch, path_arch='', solo_pssm=True, pseudocounts=0.5):
         return m, pwm, pssm
 
 
-def abrir_rnaseq(nom_arch, path_arch='', sep='\t', ext='.csv', id_col=0, updown_col=1):
+def abrir_rnaseq(nom_arch, path_arch='', sep='\t', ext='.csv', id_col=0, updown_col=1, traducir=[], header=True):
     '''Funcion que abre archivos con output de estudios de RNA-seq y devuelve listas de genes con numeros de up- o downregulacion.
     id_col determina la columna del identificador del gen.
-    updown_col determina la columna del numero correspondiente a up- o downregulacion.'''
+    updown_col determina la columna del numero correspondiente a up- o downregulacion.
+    traducir se usa para traducir nombres de genes extraidos de archivo nom_rnaseq. Si es una lista o string vacio, no se traduce. 
+    Si traducir tiene elementos, tiene que tener el numero de la columna con el nombre del gen y genoma usado para confirmar Ensembl ID (col_gene_name, genoma).'''
 
     # Inicializo la matriz que se devuelve
     M_out = []; 
+    # Inicializo booleano skip_header
+    skip_header = header; 
     # Defino la direccion del archivo con nom_arch y path_arch
     if path_arch=='':
         filepath = nom_arch + ext; 
     else:
         filepath = os.path.join(path_arch, nom_arch + ext); 
+    ### Display
+    # Inicializo contador de genes encontrados y genes totales
+    cont_encontrados = 0; 
+    cont_totales = 0; 
+    ###
     # Abro el archivo filepath
     with open(filepath, 'r') as f_out:
         print('Archivo ' + nom_arch + ' abierto.')
         # Recorro cada fila
         for curr_line in f_out.readlines():
-            # Transformo la linea en lista
-            l_line = curr_line.rstrip().split(sep=sep); 
-            # Inicializo una lista de valores que se devuelven
-            l_out = []; 
-            # Agrego los valores importantes a devolver a l_out
-            l_out.append(str(l_line[id_col])); 
-            l_out.append(float(l_line[updown_col])); 
-            # Cargo l_line en M_out
-            M_out.append(l_line[:]); 
+            # Veo si salteo header
+            if skip_header:
+                skip_header=False; 
+            else:
+                # Transformo la linea en lista
+                l_line = curr_line.rstrip().split(sep=sep); 
+                # Veo si traducir contiene elementos, si no contiene, guardo ids normalmente
+                if len(traducir)==0:
+                    # Inicializo una lista de valores que se devuelven
+                    l_out = []; 
+                    # Veo que l_line[id_col] tenga algo
+                    if str(l_line[id_col]) != '':
+                        # Agrego los valores importantes a devolver a l_out
+                        l_out.append(str(l_line[id_col])); 
+                        l_out.append(float(l_line[updown_col])); 
+                        # Cargo l_line en M_out
+                        M_out.append(l_line[:]); 
+                        # Sumo 1 a gen encontrado si se encontro un ID
+                        cont_encontrados+=1; 
+                    # Sumo 1 a genes totales aunque no se haya encontrado un ID
+                    cont_totales+=1; 
+                # Si contiene elementos, trato de buscar los ensembl IDs correspondientes a los nombres de genes
+                else:
+                    # Inicializo la lista de genes que se agrega a M_out
+                    M_add = []; 
+                    # Inicializo un booleano de genes encontrados
+                    gen_encontrado = False; 
+                    # Defino el ID de entrez y el nombre del gen
+                    entrez_id = l_line[id_col]; 
+                    gene_name = l_line[traducir[0]]; 
+                    genoma = traducir[1]; 
+                    # Uso .gene_ids_of_gene_name() para ver si el gen aparece en el genoma
+                    try:
+                        l_genes = genoma.gene_ids_of_gene_name(gene_name); 
+                        # Veo si hay genes en l_genes
+                        for curr_gen in l_genes:
+                            gen_encontrado=True; 
+                            # Inicializo la lista que se agrega a M_add
+                            l_out = []; 
+                            # Agrego los valores importantes a devolver
+                            l_out.append(str(curr_gen)); 
+                            l_out.append(float(l_line[updown_col])); 
+                            # Agrego l_out a M_add
+                            M_add.append(l_out[:]); 
+                    except:
+                        # Tiro error si no se encontro nada
+                        print('No se encontro ningun gen con gene_name ' + str(gene_name))
+                    # Agrego M_add a M_out
+                    M_out = M_out + M_add; 
+                    # Sumo 1 a gen encontrado si se encontro un ID
+                    if gen_encontrado:
+                        cont_encontrados+=1; 
+                    # Sumo 1 a genes totales aunque no se haya encontrado un ID
+                    cont_totales+=1; 
+    ### Display
+    print('Genes totales: ' + str(cont_totales))
+    print('Genes traducidos: ' + str(cont_encontrados))
+    print('Genes encontrados: ' + str(len(M_out)))
+    ###
     return M_out
 
 
